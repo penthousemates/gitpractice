ffa2732d0b066965283c8e16a32f8b9cb9fe441b
khajashaik7868 <khajashaik7868@gmail.com>
2022-01-20 08:39:44 +0000

    Revert "Third commit"
    This reverts commit 42b5bdef769f0389f41876e40766e7f0cd0f51a5.

--- sunny.sh
+++
@@ -1,5 +0,0 @@
- same lazy guy iam typing this 
-khvyfchb.uh;iohoigljb, ml;m;lmlmvm;kjfead cl.cs
-ckl vskn lkmos mklm mlm ;,n;ns  dm msvms vs
-csLK   JVJS V  LKNO;SH S MBLKB SIBHCS DIH NK C  O OJ 
-updated this line khaja
